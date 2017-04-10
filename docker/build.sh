604ecf018d74b53237d6afcb26d754e758809606
shu <shu65>
2017-04-11 00:22:29 +0900

    fixed Dockerfile and build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker build -t astral_roc .
+IMAGE_NAME=shu65/astralroc
+IMAGE=${IMAGE_NAME}:latest
+
+echo "image = ${IMAGE}"
+cd ../
+docker build -t ${IMAGE} -f ./docker/Dockerfile .
